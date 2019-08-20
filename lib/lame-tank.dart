dbabeb1687471f6a3d94bcc09c4802184a8c1c4d
Japa Alekhin Llemos <jap@alekhin.io>
2019-08-20 10:13:20 +0800

    draw background grass

--- lib/lame-tank.dart
+++ lib/lame-tank.dart
@@ -4,7 +4,21 @@ import 'package:flame/game.dart';
 class LameTank extends Game {
   Size screenSize;
 
-  void render(Canvas c) {}
+void render(Canvas c) {
+  if (screenSize == null) {
+    return;
+  }
+
+  c.drawRect(
+    Rect.fromLTWH(
+      0,
+      0,
+      screenSize.width,
+      screenSize.height,
+    ),
+    Paint()..color = Color(0xff27ae60),
+  );
+}
 
   void update(double t) {}
 
