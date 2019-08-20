acaf8e0d6d7a08193d66182fa7c7898003312c2b
Japa Alekhin Llemos <jap@alekhin.io>
2019-08-20 09:24:37 +0800

    added screensize variable added resize handler

--- lib/lame-tank.dart
+++ lib/lame-tank.dart
@@ -2,7 +2,11 @@ import 'dart:ui';
 import 'package:flame/game.dart';
 
 class LameTank extends Game {
+  Size screenSize;
+
   void render(Canvas c) {}
 
   void update(double t) {}
+
+  void resize(Size size) => screenSize;
 }
