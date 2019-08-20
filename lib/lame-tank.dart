eeadbec8a6c5e36693eda17e59c9a703f083e396
Japa Alekhin Llemos <jap@alekhin.io>
2019-08-20 16:21:52 +0800

    added tank and bullet functionality

--- lib/lame-tank.dart
+++ lib/lame-tank.dart
@@ -1,26 +1,81 @@
 import 'dart:ui';
 import 'package:flame/game.dart';
+import 'package:lame_tank/components/bullet.dart';
+import 'package:lame_tank/components/tank.dart';
 
 class LameTank extends Game {
   Size screenSize;
+  Tank tank;
+  List<Bullet> bullets;
 
-void render(Canvas c) {
-  if (screenSize == null) {
-    return;
+  void render(Canvas c) {
+    // just end execution if there's no screenSize
+    if (screenSize == null) {
+      return;
+    }
+
+    // draw a grass field
+    c.drawRect(
+      Rect.fromLTWH(
+        0,
+        0,
+        screenSize.width,
+        screenSize.height,
+      ),
+      Paint()..color = Color(0xff27ae60),
+    );
+
+    // draw the tank
+    tank.render(c);
+
+    // draw bullets
+    bullets.forEach((Bullet b) {
+      b.render(c);
+    });
   }
 
-  c.drawRect(
-    Rect.fromLTWH(
-      0,
-      0,
-      screenSize.width,
-      screenSize.height,
-    ),
-    Paint()..color = Color(0xff27ae60),
-  );
-}
+  void update(double t) {
+    if (screenSize == null) {
+      return;
+    }
 
-  void update(double t) {}
+    // make bullets fly
+    bullets.forEach((Bullet b) {
+      b.update(t);
+    });
 
-  void resize(Size size) => screenSize = size;
+    // remove off-screen bullets
+    bullets.removeWhere((Bullet b) {
+      return b.isOffscreen;
+    });
+  }
+
+  void resize(Size size) {
+    screenSize = size;
+
+    if (tank == null) {
+      tank = Tank(
+        this,
+        position: Offset(
+          screenSize.width / 2,
+          screenSize.height / 2,
+        ),
+      );
+    }
+
+    if (bullets == null) {
+      bullets = List<Bullet>();
+      shoot();
+    }
+  }
+
+  void shoot() {
+    bullets.add(
+      Bullet(
+        this,
+        position: tank.getBulletOffset(),
+        direction: tank.direction,
+      ),
+    );
+  }
 }
